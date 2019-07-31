a085a85329f37250fecd8077de4d8686481cc015
killculator <axelexp2@gmail.com>
2019-07-31 17:58:15 -0500

    arguments should be passed in the correct order

--- amort.py
+++ amort.py
@@ -12,18 +12,28 @@ def calculate_period(principal, rate, payment):
     print('in method')
     period = np.math.log((payment/(payment-rate*principal)), 1+rate)
     print(period)
+    return period
 
 def calculate_payment(principal, rate, period):
-    payment = principal*(rate*numpy.power((1+rate),period)/(numpy.power((1+rate),period)-1))
+    payment = principal*(rate*np.power((1+rate),period)/(np.power((1+rate),period)-1))
     print(payment)
+    return payment
 
+def graph_amort_table(principal, rate, payment, period):
+    while period >= 0:
+        print('table iteration goes here')
+        period = period - 1
 
 if __name__ == "__main__":
     principal, rate, payment, period = read_params()
 
     if period == '':
         print('calculating period...')
-        calculate_period(float(principal), float(rate), float(payment))
+        period = calculate_period(float(principal), float(rate), float(payment))
     elif(payment == ''):
         print('calculating payment...')
-        calculate_payment(float(principal), float(rate), float(period))
+        payment = calculate_payment(float(principal), float(rate), float(period))
+    #TODO: calculate_principal, calculate_rate
+
+#    graph_amort_table(float(principal), float(rate), float(payment),
+#            float(period))
