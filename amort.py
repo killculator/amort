5452e6e3829f14d0186702a632a403a0253ba22f
killculator <axelexp2@gmail.com>
2019-08-01 19:06:18 -0500

    amortization graph should have amortization graph

--- amort.py
+++ amort.py
@@ -2,27 +2,48 @@ import numpy as np
 
 def read_params():
     principal = input('enter principal: ')
-    rate = input('enter rate (as decimal): ')
+    rate = input('enter annual rate (as decimal): ')
     payment = input('enter payment: ')
     period = input('enter period (in months): ')
-    print(principal, rate, payment, period)
+    print('[', principal, rate, payment, period, ']')
     return principal, rate, payment, period
 
 def calculate_period(principal, rate, payment):
-    print('in method')
     period = np.math.log((payment/(payment-rate*principal)), 1+rate)
-    print(period)
+    print('[', '%.2f' % period, ']')
     return period
 
 def calculate_payment(principal, rate, period):
     payment = principal*(rate*np.power((1+rate),period)/(np.power((1+rate),period)-1))
-    print(payment)
+    print('[', '%.2f' % payment, ']')
     return payment
 
+def calculate_principal(rate, payment, period):
+    principal = (payment/rate)*(1-np.power(1+rate, -1*period))
+    print('[', '%.2f' % principal, ']')
+    return principal
+
+#keeping in case a calculation is found
+#def calculate_rate(principal, payment, period):
+#    rate = 1
+#    print(rate)
+#    return rate
+
 def graph_amort_table(principal, rate, payment, period):
-    while period >= 0:
-        print('table iteration goes here')
+    rate = rate/12
+    print('--period--+principal-+-interest-')
+    print('----------+----------+----------')
+    while (period >= 0) and (principal > 0):
+        principal, interest = calc_graph_iteration(principal, rate, payment)
+        print(int(period), ' | ', '%.2f' % principal, ' | ', '%.2f' % interest)
+        print('----------+----------+----------')
         period = period - 1
+
+def calc_graph_iteration(principal, rate, payment):
+   principal = principal - payment
+   interest = principal * rate
+   principal = principal + interest
+   return principal, interest
 
 if __name__ == "__main__":
     principal, rate, payment, period = read_params()
@@ -33,7 +54,16 @@ if __name__ == "__main__":
     elif(payment == ''):
         print('calculating payment...')
         payment = calculate_payment(float(principal), float(rate), float(period))
-    #TODO: calculate_principal, calculate_rate
+    elif(principal == ''):
+        print('calculating principal...')
+        #fix this
+        principal = calculate_principal(float(rate), float(period),
+                float(period))
+    elif(rate == ''):
+        while rate == '':
+            print('rates are arbitrarily set by the fed, so i can\'t " \
+                    "determine these, please enter in your own')
+            rate = input('enter rate (as decimal): ')
 
-#    graph_amort_table(float(principal), float(rate), float(payment),
-#            float(period))
+    graph_amort_table(float(principal), float(rate), float(payment),
+            float(period))
